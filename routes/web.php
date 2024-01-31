8f5f8cab6aa6cf6e95be007a85e0dc62fc54372a
jehd <jehd.jehzer@gmail.com>
2024-02-01 00:00:32 +0800

    adding product.blade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/shop', function () {return view('welcome');});
+
+Route::get('/product', [ProductController::class, 'products']);
