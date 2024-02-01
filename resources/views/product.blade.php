4213b685ffe4800b8891f1e4675ff10024604d51
jehd <jehd.jehzer@gmail.com>
2024-02-01 17:18:12 +0800

    Designing Modal

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -53,18 +53,55 @@
                             
 
                             <!-- Modal -->
-                            <div class="modal modal-lg fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
-                                        <div class="modal-body p-0 d-flex justify-content-center">
-                                            <img src="img/2086922454.jpg" class="card-img-top" alt="...">
-                                            <img src="img/2086922454.jpg" class="card-img-top" alt="...">
-                                            <img src="img/2086922454.jpg" class="card-img-top" alt="...">
+                                        <div class="text-end">
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        
+                                        <div class="modal-body">
+                                            <div class="row">
+                                                <div class="col-6 house-pic">
+                                                    <img src="img/2086922454.jpg" class="card-img-top" alt="...">
+                                                    <img src="img/2086922454.jpg" class="card-img-top" alt="...">
+                                                    <img src="img/2086922454.jpg" class="card-img-top" alt="...">
+                                                </div>
+                                                <div class="col-6">
+                                                    <div class="d-flex flex-column">
+                                                        <div>
+                                                            <input type="checkbox" name="3d" id="3d" value="500">
+                                                            <label for="3d">3D Perspective</label>
+                                                        </div>
+                                                        <div>
+                                                            <input type="checkbox" name="floorplan" id="floorplan" value="400">
+                                                            <label for="floorplan">Floor Plans</label>
+                                                        </div>
+                                                        <div>
+                                                            <input type="checkbox" name="interior" id="interior" value="300">
+                                                            <label for="interior">Interior</label>
+                                                        </div>
+                                                        <div>
+                                                            <input type="checkbox" name="animaition" id="animation" value="1000">
+                                                            <label for="animation">Animation</label>                                                        
+                                                        </div>
+                                                        
+                                                    </div>
+
+                                                    <div class="mt-2">
+                                                        <h3>Php 5,000</h3>
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
                                         </div>
                                         <div class="modal-footer">
+                                            <div>
+                                                <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, aut?</h5>
+                                            </div>
                                             <div class="container d-flex justify-content-around">
-                                                <button><img src="img/icons/peso-icon.svg" alt="">BUY NOW</button>
-                                                <button><img src="img/icons/cart-icon.svg" alt="">ADD TO CART</button>
+                                                <button><img src="img/icons/peso-icon.svg" alt=""><span>BUY NOW</span></button>
+                                                <button><img src="img/icons/cart-icon.svg" alt=""><span>ADD TO CART</span></button>
                                             </div> 
                                         </div>
                                     </div>
