b61e899a52f147d90be1be1afacd6339818dfaf4
jehd <jehd.jehzer@gmail.com>
2024-02-03 17:16:16 +0800

    finished design of navbar and footer

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,10 +5,14 @@
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/navbar.css">
     <link rel="stylesheet" href="/css/products.css">  
-    <!-- <link rel="stylesheet" href="/css/footer.css">   -->
+    <link rel="stylesheet" href="/css/footer.css">  
+
+    
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <script src="/js/hide_notif.js"></script>
+
+    <!-- external js -->
+    <script src="/js/navbar.js"></script>
