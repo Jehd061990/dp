d3ec9cde9509d436b61f8fc199b276d29058303a
jehd <jehd.jehzer@gmail.com>
2024-02-07 10:34:31 +0800

    Fixed Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,6 @@
 <html lang="en">
 
 <head>
-    <!-- @include('layouts/head')
-    <link rel="stylesheet" href="css/styles.css" />
-    <link rel="stylesheet" href="/css/navbar.css">
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/css/home.css">
-    <script src="/js/home.js"></script>  -->
-
     @include('layouts/header')
 
     <title>Home</title>
@@ -19,34 +12,28 @@
     
     <div class="home_body">
     <div id="dwelling_carousel">
-        <!-- <div><img src="/img/2056374.jpg" alt="3D House Perspective" class="img-fluid dwelling_car"/></div> -->
-        <!-- <div class="row dwelling_car"> -->
-            <!-- <div class="col-lg-12"> -->
-                    <div id="dwelling-hero" class="carousel slide" data-bs-ride="carousel" data-bs-interval="6000">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="/img/2056374.jpg" alt="3D perspective" class="img-fluid img_home">
-                                <div class="carousel-caption"></div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/img/her_2.jpg" alt="3D perspective" class="img-fluid img_home">
-                                <div class="carousel-caption"></div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/img/her_1.jpg" alt="3D perspective" class="img-fluid img_home">
-                                <div class="carousel-caption"></div>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" id="previous" type="button" data-bs-target="#dwelling-hero" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon"></span>
-                        </button>
-                        <button class="carousel-control-next next" id="next"  type="button" data-bs-target="#dwelling-hero" data-bs-slide="next">
-                            <span class="carousel-control-next-icon"></span>
-                        </button>
-                    </div>
-               
-            <!-- </div> -->
-        <!-- </div> -->
+        <div id="dwelling-hero" class="carousel slide" data-bs-ride="carousel" data-bs-interval="6000">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/img/2056374.jpg" alt="3D perspective" class="img-fluid img_home">
+                    <div class="carousel-caption"></div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/img/her_2.jpg" alt="3D perspective" class="img-fluid img_home">
+                    <div class="carousel-caption"></div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/img/her_1.jpg" alt="3D perspective" class="img-fluid img_home">
+                    <div class="carousel-caption"></div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" id="previous" type="button" data-bs-target="#dwelling-hero" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next next" id="next"  type="button" data-bs-target="#dwelling-hero" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </button>
+        </div>
     </div>
     
     <div class="container">
