a473292bcae82b9cb855e57d44659d878b2750da
jehd <jehd.jehzer@gmail.com>
2024-02-08 17:48:08 +0800

    Initial code for super admin dashboard page

--- resources/views/superadmin_dashboard.blade.php
+++ resources/views/superadmin_dashboard.blade.php
@@ -8,9 +8,32 @@
 
 <body class="d-flex justify-content-between">
     @include('layouts/navbar_superadmin')
-    <div class="bg-primary content-body">
-        <h1>WELCOME SUPERADMIN</h1>
-        <!-- <a href="/logout">Logout</a> -->
+    <div class="content-body pt-5 me-3" id="sa-dashboard-page">
+        <div class="d-flex justify-content-between gap-4">
+            <div class="left">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 bg-primary align-middle">
+                            hello
+                        </div>
+
+                        <div class="col-lg-4 bg-secondary">
+                            hi
+                        </div>
+
+                        <div class="col-lg-4 bg-success">
+                            World
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="right">World</div>
+        </div>
+        <div class="bar-chart mt-4">
+            <!-- <div class="bar-chart">
+
+            </div> -->
+        </div>
     </div>
 </body>
 
